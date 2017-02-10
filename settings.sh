37638b0da081026cc85a804a7ce10e3cc8b42537
tolomaus <niek.bartholomeus@gmail.com>
2017-02-10 23:15:25 +0100

    changed location of log files

--- settings.sh
+++ settings.sh
@@ -2,6 +2,7 @@
 SOURCE_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 APP_DIR=~/apps/languagedetector
 
-LOG_DIR=~/tmp/logs
+LOG_DIR=~/logs
+mkdir -p $LOG_DIR
 
 SPARK_HOME=~/apps/spark-2.1.0-bin-hadoop2.7
