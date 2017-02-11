c5fa0423c6fc93be5ca968a4581a3d8547bb5cf4
tolomaus <niek.bartholomeus@gmail.com>
2017-02-11 17:45:03 +0100

    add spark notebook install to provisioning script

--- scripts/provision_centos.sh
+++ scripts/provision_centos.sh
@@ -11,13 +11,17 @@ SPARK_VERSION=2.1.0
 wget -P /tmp http://apache.belnet.be/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop2.7.tgz
 mkdir -p $SPARK_HOME
 
-tar xvf /tmp/spark-$SPARK_VERSION-bin-hadoop2.7.tgz -C /opt
-#cp /opt/spark/conf/spark-settings.sh /opt/spark-$SPARK_VERSION-bin-hadoop2.7/conf
-ln -fs /opt/spark-$SPARK_VERSION-bin-hadoop2.7 $SPARK_HOME
+tar xvf /tmp/spark-$SPARK_VERSION-bin-hadoop2.7.tgz -C $SPARK_HOME
+ln -fsn $SPARK_HOME/spark-$SPARK_VERSION-bin-hadoop2.7 $SPARK_HOME
 mkdir -p /tmp/spark-events
 echo "Done."
 
 echo "Installing sbt..."
 curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
 yum install -y sbt
+echo "Done."
+
+echo "Installing Spark Notebook..."
+wget -P /tmp http://spark-notebook.io/dl/tgz/0.7.0/2.11/2.1.0/2.7.3/false/true
+tar xvf /tmp/spark-notebook-0.7.0-scala-2.11.8-spark-2.1.0-hadoop-2.7.3.tgz
 echo "Done."
