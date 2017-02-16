a736946eb43d3b5f2c213a8ed1c0bb7687ff7cf5
tolomaus <niek.bartholomeus@gmail.com>
2017-02-16 19:02:46 +0100

    export data deps - test.comnf should come from file location and not resource

--- scripts/export_data_dependencies.sh
+++ scripts/export_data_dependencies.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+. $(dirname $0)/../settings.sh
+
+ENV=test
+
 cd $(dirname $0)/..
-sbt -Dconfig.resource=test.conf "run-main biz.meetmatch.util.DataDependencyPrinter"
+sbt -Dconfig.file=${APP_DIR}/conf/${ENV}.conf "run-main biz.meetmatch.util.DataDependencyPrinter"
 cd -
