474b1684667ea9263841cfe7763c815c0716f6ed
tolomaus <niek.bartholomeus@gmail.com>
2017-02-26 19:21:19 +0100

    fix and improve debugging and monitoring scripts

--- scripts/spark_submit_with_debugging.sh
+++ scripts/spark_submit_with_debugging.sh
@@ -7,4 +7,4 @@ shift;
 
 echo "Start an IntelliJ remote debugging session to port 5005"
 # add the dependencies to the class path using "--driver-class-path" otherwise the guava lib v14.0 from spark is taken while languagedetect needs >= 18.0
-${SPARK_HOME}/bin/spark-submit --jars ${APP_ENV_DIR}/languagedetector-deps.jar --class biz.meetmatch.$CLASS --driver-memory 2g --driver-java-options "-Dconfig.file=${APP_CONFIG} -DbusinessLogFileName=${LOG_DIR}/${BUSLOGFILE} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005" --driver-class-path "${APP_ENV_DIR}/languagedetector-deps.jar" ${APP_ENV_DIR}/languagedetector.jar "$@"
+${SPARK_HOME}/bin/spark-submit --jars ${APP_ENV_DIR}/${APP_NAME}-deps.jar --class biz.meetmatch.$CLASS --driver-memory 2g --driver-java-options "-Dconfig.file=${APP_CONFIG} -DbusinessLogFileName=${LOG_DIR}/${BUSLOGFILE} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005" --driver-class-path "${APP_ENV_DIR}/${APP_NAME}-deps.jar" ${APP_ENV_DIR}/${APP_NAME}.jar "$@"
--- scripts/spark_submit_with_monitoring.sh
+++ scripts/spark_submit_with_monitoring.sh
@@ -5,6 +5,8 @@ set -e
 shift;
 shift;
 
-echo "Start a VisualVM JMX session to myservername.com:9080"
+HOSTNAME="localhost" // set this to the name of the server on which the spark app runs
+
+echo "Start a VisualVM JMX session to ${HOSTNAME}:8090"
 # add the dependencies to the class path using "--driver-class-path" otherwise the guava lib v14.0 from spark is taken while languagedetect needs >= 18.0
-${SPARK_HOME}/bin/spark-submit --jars ${APP_DIR}/languagedetector-assembly-1.0-deps.jar --class biz.meetmatch.$CLASS --driver-memory 2g --driver-java-options "-Dconfig.file=${APP_CONFIG} -DbusinessLogFileName=${LOG_DIR}/${BUSLOGFILE} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 -Dcom.sun.management.jmxremote.rmi.port=8091 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=myservername.com" --driver-class-path "${APP_DIR}/languagedetector-assembly-1.0-deps.jar" ${APP_DIR}/languagedetector_2.11-1.0.jar "$@"
+${SPARK_HOME}/bin/spark-submit --jars ${APP_ENV_DIR}/${APP_NAME}.jar --class biz.meetmatch.$CLASS --driver-memory 2g --driver-java-options "-Dconfig.file=${APP_CONFIG} -DbusinessLogFileName=${LOG_DIR}/${BUSLOGFILE} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 -Dcom.sun.management.jmxremote.rmi.port=8091 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=${HOSTNAME}" --driver-class-path "${APP_ENV_DIR}/${APP_NAME}-deps.jar" ${APP_ENV_DIR}/${APP_NAME}.jar "$@"
